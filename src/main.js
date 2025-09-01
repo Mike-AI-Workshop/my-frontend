3700a50afad65ce8e459accc5619fdf92241600f
Mike-AI-Workshop <daigaoyuan123@gmail.com>
2025-09-01 11:22:23 +0800

    删掉了最基础的 style,然后将我的 main 函数修改了一下,挂载上了我的路由

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+//创建应用实例
+const app = createApp(App)
+
+//在挂载到页面之前，先安装路由模块
+app.use(router)
+
+//最后再挂载到页面上
+app.mount('#app')
