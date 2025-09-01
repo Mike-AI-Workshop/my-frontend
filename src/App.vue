a2cf84f5ba8a970fe54b3e2772ffa8bcb8f867cb
Mike-AI-Workshop <daigaoyuan123@gmail.com>
2025-09-01 15:05:45 +0800

    出现了莫名其妙的 bug,我的5173端口用不了,只好暂时改为5174，先用着

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import TheHeader from './components/TheHeader.vue';
     <div id="app-container">
         <TheHeader />
 
-        <main id="main-container">
+        <main class="main-container">
             <router-view /> 
         </main>
 
@@ -17,11 +17,11 @@ import TheHeader from './components/TheHeader.vue';
 
 <style scoped>
 #app-container{
-    display:flex;
+    display: flex;
     flex-direction: column;
     min-height: 100vh;
 }
 .main-container{
-    flex-grow:1;
+    flex-grow: 1;
 }
 </style>
