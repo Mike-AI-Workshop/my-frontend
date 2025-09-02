ffcc90ca2e714e4d6c14c6414b8828064a086999
Mike-AI-Workshop <daigaoyuan123@gmail.com>
2025-09-02 22:00:43 +0800

    问题修复,并且增加了淡入淡出

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <script setup>
+import gsap from 'gsap';
 import TheFooter from './components/TheFooter.vue';
 import TheHeader from './components/TheHeader.vue';
+
+// GSAP Animation Hooks
+const onEnter = (el, done) => {
+  gsap.fromTo(el, 
+    { opacity: 0 }, 
+    { opacity: 1, duration: 0.5, onComplete: done }
+  );
+};
+
+const onLeave = (el, done) => {
+  gsap.to(el, 
+    { opacity: 0, duration: 0.5, onComplete: done }
+  );
+};
 </script>
 
 <template>
-    <div id="app-container">
-        <TheHeader />
+  <div id="app-container">
+    <TheHeader />
 
-        <main class="main-container">
-            <router-view /> 
-        </main>
+    <main class="main-container">
+      <RouterView v-slot="{ Component }">
+        <Transition mode="out-in" @enter="onEnter" @leave="onLeave">
+          <component :is="Component" />
+        </Transition>
+      </RouterView>
+    </main>
 
-        <TheFooter />
-    </div>
+    <TheFooter />
+  </div>
 </template>
 
 <style scoped>
